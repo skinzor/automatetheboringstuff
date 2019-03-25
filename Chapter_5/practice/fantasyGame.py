f5ac367ffe3c0f6ae8a6a83370ace54f7cc46ed5
skinzor <robertpopescu95@yahoo.com>
2019-03-25 20:48:38 +0200

    Chapter 5: fantasyGame.py
    * Replace the code of displayIventory function with the code which was already done.
    * There's no need for displayInventory function to have two parameters.
    * By using the parameter for inventory.items(), the function can be called with any dictionary list and nothing has to be modified in the function itself.
    
    * Add the addToInventory function in the same python file because the practice doesn't mention to create a new file and because the displayInventory can now be used for any dictionary list without modifying the function,
    
    * Still keep inventory.py and dragonLoot.py.

--- Chapter_5/practice/fantasyGame.py
+++ Chapter_5/practice/fantasyGame.py
@@ -2,15 +2,14 @@
 
 stuff = {}
 
-def displayInventory(i, a):
-    totalItems = 0
-    print('')
-    print('Inventory:')
-    
-    for i, a in stuff.items():
-        print(str(a) + ' ' + i)
-        totalItems = totalItems + a
-    print('Total number of items: ' + str(totalItems))
+def displayInventory(inventory):
+    item_total = 0
+    print(' ')
+    print("Inventory:")
+    for k, v in inventory.items():
+        print(str(v) + ' ' + k)
+        item_total = item_total + v
+    print("Total number of items: " + str(item_total))
 
 
 # Ask 5 items and the amount of each item.
@@ -23,4 +22,29 @@ while len(stuff) != 5:
     stuff.setdefault(item, amount)
     # print(stuff)
 
-displayInventory(item, amount)
+displayInventory(stuff)
+
+
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+
+def addToInventory(inventory, addedItems):
+    # your code goes here
+    totalLoot = len(dragonLoot)
+    for i in range (0, totalLoot):
+        print(i)
+        if dragonLoot[i] in inv:
+            inv[dragonLoot[i]] = inv[dragonLoot[i]] + 1
+        else:
+            inv.setdefault(dragonLoot[i], 1)
+    return inv
+    
+    
+
+
+inv = addToInventory(inv,dragonLoot)
+print(inv)
+displayInventory(inv)
